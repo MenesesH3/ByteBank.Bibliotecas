40a075fa57da51eee08548bf512749e28ce52c66
Guilherme <Guilherme@GUILHERME>
2022-02-27 02:11:58 -0300

    Adicionar arquivos de projeto.

---
+++ ByteBank.SistemaAgencia/Estagiario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class Estagiario 
+    {
+        //public Estagiario(double salario, string cpf)
+        //    :base(salario, cpf)
+        //{
+
+        //}
+
+        //public override void AumentarSalario()
+        //{
+        //    // Qualquer código
+        //}
+
+        //protected override double GetBonificacao()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+DateTime dataFimPagamento = new DateTime(2018, 6, 30);
+DateTime dataCorrente = DateTime.Now;
+
+TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+Console.WriteLine(mensagem);
+
+Console.ReadLine();
